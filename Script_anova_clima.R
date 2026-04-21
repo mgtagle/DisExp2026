04a35e4fff004f5a16b1d70238645300e2a53826
Marco <marco.gonzaleztg@uanl.edu.mx>
2026-04-21 09:54:45 -0600

    update

--- Script_anova_clima.R
+++ Script_anova_clima.R
@@ -2,11 +2,14 @@
 # 14/04/2026
 # Datos climatológicos
 # Cumbres NL, Villagran Tamps, Carranza Coahuila
+
 library(lubridate)
+library(hms)
 clima <- read.csv("Climatologia.csv", header =T)
 clima$Localidad <- as.factor(clima$Localidad)
 clima$Fecha <- as.Date(clima$Fecha, format = "%d/%m/%Y")
-# clima$Hora <- as.POSIXct(clima$Hora, format = "%H:%M:%S")
+clima$Hora <- hms::as_hms(clima$Hora)
+
 
 boxplot(clima$TEMP~ clima$Localidad,
         col= "indianred",
@@ -17,3 +20,37 @@ boxplot(clima$TEMP~ clima$Localidad,
         outpch = 19) # cambiar la forma a circulo relleno
 
 tapply(clima$TEMP, clima$Localidad, mean)
+
+# Temperatura en el curso del semana
+
+cumbres <- subset(clima,Localidad =="Cumbres")
+cumbres <- cumbres[order(cumbres$fechaHora),]
+cumbres$Hora <- as.character(cumbres$Hora)
+clima$FechaHora <- as.POSIXct(paste(clima$Fecha, clima$Hora),
+                              format = "%Y-%m-%d %H:%M:%S")
+
+plot(cumbres$FechaHora , cumbres$TEMP,
+     type = "l",
+     col = "indianred",
+     lwd =2,
+     xaxt = "n",
+     xlab = "Localidad EMAs",
+     ylab  = "Temperatura (°C)")
+
+# Normalidad de adtos
+tapply(clima$TEMP, clima$Localidad, mean)
+shapiro.test(clima$TEMP)
+
+
+
+# ANOVA
+
+clim.aov <- aov(clima$TEMP ~ clima$Localidad)
+summary(clim.aov)
+
+# Aceptamos H1, por lo tanto realizamos una prueba de Tukey
+TukeyHSD(clim.aov)
+
+# Homogeneidad de varianzas
+tapply(clima$TEMP, clima$Localidad, var)
+bartlett.test(clima$TEMP ~ clima $Localidad)
